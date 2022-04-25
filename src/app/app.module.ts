4c5ecfebefd9c212c69abb87cfee4c69f97f504d
Omar <omarkhaled2904@gmail.com>
2022-04-25 23:24:28 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,55 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatTableModule} from '@angular/material/table';
+import {MatSelectModule} from '@angular/material/select';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ProfileComponent } from './profile/profile.component';
+import { GradeComponent } from './grade/grade.component';
+import { GradesComponent } from './grades/grades.component';
+import { AnnouncementsComponent } from './announcements/announcements.component';
+import { AnnouncementComponent } from './announcement/announcement.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomePageComponent,
+    ProfileComponent,
+    GradeComponent,
+    GradesComponent,
+    AnnouncementsComponent,
+    AnnouncementComponent,
+    CarouselComponent,
+    LoginComponent,
+    RegisterComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatCardModule,
+    MatGridListModule,
+    MatTableModule,
+    MatInputModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
